701ad86d4399206ac8a2b9d5c4591f93288d29cb
Alexandr <nistkasten@mail.ru>
2020-10-09 23:46:36 +0700

    adapted the site to all devices

--- js/app.js
+++ js/app.js
@@ -1,7 +1,53 @@
-$('.sl').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  adaptiveHeight: false ,
-    mobileFirst:true
+$(function(){
+    $('.slider__inner').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      speed:300,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      adaptiveHeight: false ,
+      mobileFirst:true,
+      dots:true,
+      arrows:false,
+      responsive:[
+            {
+            breakpoint: 768,
+            settings:{
+            dots: false,
+            arrows:true,
+            adaptiveHeight:true
+
+            }
+
+            }
+        ]
+
+    });
+$('#nav__toggle').on('click',function(event){
+    event.preventDefault();
+    $(this).toggleClass('active');
+    $('.nav__link').toggleClass('active');
     
 });
+
+let sliderH =+ $('#slider').innerHeight(),
+    scrollOffset = $(window).scrollTop();
+    checkscroll(scrollOffset);
+
+    $(window).on('scroll', function(){
+        scrollOffset = $(this).scrollTop();
+        checkscroll(scrollOffset);
+    });
+    
+    function checkscroll(scrollOffset){
+        
+        if(scrollOffset >= sliderH){
+            $('#nav').addClass('fixed');
+        }else{
+            $('#nav').removeClass('fixed');
+        }
+
+    }   
+    
+});
+
